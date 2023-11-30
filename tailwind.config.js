17f539bbc585ba7570105c28627cdee7fbd03e36
sejal-kale <75160356+sejal-kale@users.noreply.github.com>
2023-11-30 22:26:42 +0530

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js}"
+    "./src/**/*.{html,js}",
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'Arvo': ['Arvo',]
+    }
   },
   plugins: [],
 }
-
