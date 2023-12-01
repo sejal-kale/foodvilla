edcdff7ccec4ee798684ed32d3dd0f8b7c3765cb
sejal-kale <75160356+sejal-kale@users.noreply.github.com>
2023-12-01 12:11:55 +0530

    Modification

--- src/utils/UserContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const UserContext = createContext({
-    user:
-    {
-    name:"Dummy Name",
-    email:"dummyname@gmail.com"
-    }
-})
--- src/utils/helper.js
+++
@@ -1,6 +0,0 @@
-// Filter fucntion
-export function filterData(searchInput,restaurants){
-    return restaurants.filter((restaurant)=>
-        restaurant.info.name.toLowerCase().includes(searchInput.toLowerCase())
-);
-}
